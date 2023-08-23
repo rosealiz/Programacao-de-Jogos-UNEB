619ab690d47e39f895ac7cbeff49f53674b99385
Luana <142853453+LuaaLuana@users.noreply.github.com>
2023-08-22 21:56:59 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -246,11 +246,9 @@ namespace JogoDaVelha
             
             Console.WriteLine("");
             Console.WriteLine("Digite a posição (1-9) que deseja marcar.");
-            Console.WriteLine("Sendo elas:\n 1 | 2 | 3");
-            Console.WriteLine("-----------");
-            Console.WriteLine(" 4 | 5 | 6");
-            Console.WriteLine("-----------");
-            Console.WriteLine(" 7 | 8 | 9");
+            Console.WriteLine("Sendo elas:\n[1] [2] [3]");            
+            Console.WriteLine("[4] [5] [6]");
+            Console.WriteLine("[7] [8] [9]");
             Console.WriteLine("Bom jogo!"); //instruções
 
             JogoVelha jogo = new JogoVelha(nomeJogadorFisico, simboloJogadorFisico);
