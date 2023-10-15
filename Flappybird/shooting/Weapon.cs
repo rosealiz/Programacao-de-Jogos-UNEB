5210f62e33b38cc853ed4ac0425638b584e0bfed
rosealiz <112977051+rosealiz@users.noreply.github.com>
2023-10-15 09:07:06 -0300

    Update Weapon.cs

--- Flappybird/shooting/Weapon.cs
+++ Flappybird/shooting/Weapon.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Weapon : MonoBehaviour
 {
     public Transform FirePoint;
     public GameObject bulletPrefab;
+    private int bulletCount = 0;
 
-    // Update is called once per frame
     void Update()
-
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
     }
-    void Shoot ()
+
+    void Shoot()
     {
-       Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        BulletMove bulletMoveScript = bullet.GetComponent<BulletMove>();
+        if (bulletMoveScript != null)
+        {
+            bulletMoveScript.SetWeaponReference(this);
+        }
+    }
+
+    public void AddCount()
+    {
+        bulletCount++;
+        Debug.Log("Bullet Count: " + bulletCount);
+        if (bulletCount == 30)
+        {
+            SceneManager.LoadSceneAsync(2);
+        }
     }
 }
