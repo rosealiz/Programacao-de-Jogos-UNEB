ab95357b0af9776c378af3588b8454658a6115a1
rosealiz <112977051+rosealiz@users.noreply.github.com>
2023-10-15 09:07:39 -0300

    Update BulletMove.cs

--- Flappybird/shooting/BulletMove.cs
+++ Flappybird/shooting/BulletMove.cs
@@ -6,10 +6,18 @@ public class BulletMove : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    private Weapon weapon;
+
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
+
+    public void SetWeaponReference(Weapon weaponReference)
+    {
+        weapon = weaponReference;
+    }
+
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Enemy enemy = hitInfo.GetComponent<Enemy>();
@@ -18,5 +26,9 @@ public class BulletMove : MonoBehaviour
             enemy.TakeDamage(34);
         }
         Destroy(gameObject);
+        if (weapon != null)
+        {
+            weapon.AddCount();
+        }
     }
 }
